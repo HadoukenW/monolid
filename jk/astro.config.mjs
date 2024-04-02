561de182247983e758cd185bf049f7a350bd4202
Egor <hadouken22338@gmail.com>
2024-04-02 10:56:12 +0300

    netlify

--- jk/astro.config.mjs
+++ jk/astro.config.mjs
@@ -1,14 +1,16 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
-
-	devToolbar: {
-		enabled: false
-	  }
+  site: 'https://example.com',
+  integrations: [mdx(), sitemap()],
+  devToolbar: {
+    enabled: false
+  },
+  output: "server",
+  adapter: netlify()
 });
