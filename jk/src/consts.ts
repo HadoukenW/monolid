9682d7664d0962f20340d61e3c5df006478f55b2
Egor <hadouken22338@gmail.com>
2024-04-18 11:47:19 +0300

    FGHRHU8

--- jk/src/consts.ts
+++ jk/src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Монолид';
+export const SITE_TITLE = 'Агенство недвижимости Монолид Челябинск';
 export const SITE_DESCRIPTION = 'Являемся компанией, специализирующейся на продаже квартир в новостройках и вторичного жилья в Челябинске. Наша главная цель – обеспечить высокий уровень сервиса, профессионализм и индивидуальный подход к каждому клиенту.';
