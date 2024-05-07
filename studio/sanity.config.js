0c1a7eb5bf60679b65c51ec44d2c981086ac5545
egor <hadouken22338@gmail.com>
2024-05-07 20:35:24 +0500

    another one yet

--- studio/sanity.config.js
+++ studio/sanity.config.js
@@ -9,6 +9,7 @@ export default defineConfig({
 
   projectId: '5d4cymjm',
   dataset: 'production',
+  token: "sktKeI484klyfjDlWpRuJI9VD3MCiTMKBCSS8pdINTaE3zfY5IAmfqegAhy2QnRYOxrdMLAvJbrrONxD1EVd7f0Pu675M4lmiFNBdFkbQIW15TvSodpS9TVrNXCE5vhUuAXH21qvWMspJvsCxzCVpvUen3rqB640pOti8I5EkVATAMtBXEip",
 
   plugins: [structureTool(), visionTool()],
 
