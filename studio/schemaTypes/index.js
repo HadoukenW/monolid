b94b4becca6e5d62e3d83b6d730d6c36211be074
egor <hadouken22338@gmail.com>
2024-05-09 04:10:03 +0500

    sanity update

--- studio/schemaTypes/index.js
+++ studio/schemaTypes/index.js
@@ -4,7 +4,16 @@ import { zhk81riverside } from "./objects/81-1-zhk-riverside";
 import { zhk82riverside } from "./objects/81-2-zhk-riverside";
 import { zhkBratievKashirinyh } from "./objects/zhk-bratiev-kashirinyh-layouts";
 import { zhkklever } from "./objects/zhk-klever";
+import { zhk4lenina } from "./objects/zhk-4-lenina";
 import { zhksportivniy } from "./objects/zhk-sportivniy";
 import { zhkvidniy } from "./objects/zhk-vidniy";
+import { zhkgoloskashirinyx } from "./objects/zhk-golos-kashirinyx";
+import { zhkkonfetti } from "./objects/zhk-konfetti";
+import { zhkniksline } from "./objects/zhk-niks-line";
+import { zhknikscity } from "./objects/zhk-niks-city";
+import { zhknovil } from "./objects/zhk-novil";
+import { zhkparkoviypremium } from "./objects/zhk-parkoviy-premium";
+import { zhkprityazhenie } from "./objects/zhk-prityazhenie";
+import { zhkhugeeitown } from "./objects/zhk-hugee-itown";
 
-export const schemaTypes = [zhkBratievKashirinyh, zhk81riverside, zhk82riverside, zhksportivniy, zhkvidniy, zhk18, zhk14, zhkklever]
+export const schemaTypes = [zhkBratievKashirinyh, zhk81riverside, zhk82riverside, zhksportivniy, zhkvidniy, zhk18, zhk14, zhkklever, zhk4lenina, zhkgoloskashirinyx, zhkkonfetti, zhkniksline, zhknikscity, zhknovil, zhkparkoviypremium, zhkprityazhenie, zhkhugeeitown]
---
+++ studio/schemaTypes/objects/zhk-4-lenina.js
@@ -0,0 +1,33 @@
+import {defineField, defineType} from 'sanity'
+
+export const zhk4lenina = defineType({
+    name: 'zhk-4-lenina',
+    title: 'ЖК 4 Ленина',
+    type: 'document',
+    fields: [
+        {
+            name: 'photo',
+            title: 'Фото планировки',
+            type: 'image'
+        },
+        {
+            name: 'rooms',
+            title: 'Количество комнат',
+            type: 'string',
+            options: {
+                list: [
+                    {title: 'студия', value: 'студия'},
+                    {title: '1-комнатная', value: '1-комнатная'},
+                    {title: '2-комнатная', value: '2-комнатная'},
+                    {title: '3-комнатная', value: '3-комнатная'},
+                    {title: '4-комнатная', value: '4-комнатная'},
+                ]
+            }
+        },
+        {
+            name: 'price',
+            title: 'Цена',
+            type: 'string'
+        }
+    ]
+})
